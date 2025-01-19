5877d9e5f139fba5a9723562f45c140ce5447e71
astro-develops <astromp777@gmail.com>
2025-01-19 11:07:54 -0500

    landing

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -7,16 +7,16 @@ export default function Home() {
         <Image
           src="/assets/header.svg"
           alt="Header"
-          width={1500}
+          width={1200}
           height={200}
           priority
         />
-        <div className="flex gap-4 mt-8">
-          <button className="btn-download px-6 py-3 rounded-lg">
+        <div className="flex gap-4 mt-4">
+          <button className="btn-download px-5 py-2 rounded-lg">
             Download
           </button>
-          <button className="btn-outline px-6 py-3 rounded-lg">
-            Learn More
+          <button className="btn-outline px-5 py-2 rounded-lg">
+            <a href="https://github.com/atomisadev/fausses-nouvelles-backend" target="_blank">Github</a>
           </button>
         </div>
       </main>
