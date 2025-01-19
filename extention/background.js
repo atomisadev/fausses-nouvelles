ccbfe5c758afdd69df3ec84d2cee84251875e112
atomisadev <atomdevelops@pm.me>
2025-01-19 11:31:12 -0500

    working thing

--- extention/background.js
+++ extention/background.js
@@ -1,7 +1,8 @@
-chrome.browserAction.onClicked.addListener(function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        var currentUrl = tabs[0].url;
-        var newUrl = 'https://faussesnouvelles.com/article?url=' + encodeURIComponent(currentUrl);
-        chrome.tabs.update(tabs[0].id, { url: newUrl });
-    });
+chrome.browserAction.onClicked.addListener(function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    var currentUrl = tabs[0].url;
+    var newUrl =
+      "http://localhost:3000/article?url=" + encodeURIComponent(currentUrl);
+    chrome.tabs.update(tabs[0].id, { url: newUrl });
+  });
 });
