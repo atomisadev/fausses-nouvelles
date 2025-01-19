e84d53b2b078ac14845cf766ebb7d6492d031277
atomisadev <atomdevelops@pm.me>
2025-01-19 09:20:00 -0500

    loading works

--- web/app/article/page.tsx
+++ web/app/article/page.tsx
@@ -4,6 +4,12 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
+import { Be_Vietnam_Pro } from "next/font/google";
+
+const beVietnamPro = Be_Vietnam_Pro({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+});
 
 interface ArticleData {
   title: string;
@@ -33,21 +39,27 @@ export default function Article() {
 
   return (
     <main className="flex min-h-screen items-center justify-center flex-col p-4">
-      {loading ? (
+      <div className="flex flex-col items-center gap-8">
         <div className="flex flex-col items-center gap-4">
-          <h1 className="font-apple-garamond font-light text-4xl">
+          <h1 className="font-apple-garamond font-light tracking-tighter text-6xl">
             fausses nouvelles
           </h1>
-          <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900" />
-          <p className="font-apple-garamond">Loading your article</p>
+          {loading && (
+            <>
+              <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900" />
+              <p className={`${beVietnamPro.className} font-semibold`}>
+                Loading your article
+              </p>
+            </>
+          )}
         </div>
-      ) : (
-        article && (
+
+        {article && (
           <motion.div
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            className="max-w-2xl w-full rounded-lg overflow-hidden shadow-lg bg-white"
+            className="max-w-xl w-[24rem] rounded-lg overflow-hidden shadow-lg bg-white"
           >
             {article.mainImage && (
               <div className="w-full h-48 relative">
@@ -60,16 +72,19 @@ export default function Article() {
               </div>
             )}
             <div className="p-6">
-              <h1 className="font-apple-garamond text-2xl mb-4">
+              <p className={`${beVietnamPro.className} mb-3 font-semibold`}>
+                Article from {article.siteName}
+              </p>
+              <h1 className="font-apple-garamond font-bold text-2xl mb-4">
                 {article.title}
               </h1>
-              <p className="font-apple-garamond text-gray-600">
-                {article.content.split(" ").slice(0, 10).join(" ")}...
+              <p className={`${beVietnamPro.className} text-gray-600`}>
+                {article.content.split(" ").slice(0, 15).join(" ")}...
               </p>
             </div>
           </motion.div>
-        )
-      )}
+        )}
+      </div>
     </main>
   );
 }
